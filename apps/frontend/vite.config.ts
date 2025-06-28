dbef783201f2378083731a91ce842272d329b29f
paweliwanicki <pawel_iwanicki@outlook.com>
2025-06-28 14:44:51 +0200

    vite config update with google cloud host

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: { alias: { '@': '/src' } },
+  resolve: { alias: { "@": "/src" } },
   plugins: [react()],
   preview: {
-    port: parseInt(process.env.PORT || '5173'),
+    port: parseInt(process.env.PORT || "5173"),
     strictPort: true,
-    host: '0.0.0.0', 
-    allowedHosts: ['*'], 
+    host: "0.0.0.0",
+    allowedHosts: ["jobs-portal-frontend-1080803823360.europe-west1.run.app", '*'],
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://backend:3000', // local nest app
+      "/api": {
+        target: "http://backend:3000", // local nest app
         changeOrigin: true,
       },
-      '/uploads': {
+      "/uploads": {
         // folder with uploaded files
-        target: 'http://backend:3000',
+        target: "http://backend:3000",
         changeOrigin: true,
       },
     },
